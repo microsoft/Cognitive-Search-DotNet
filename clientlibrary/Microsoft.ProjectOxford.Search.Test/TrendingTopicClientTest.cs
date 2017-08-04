7576f49226f207c2b342de035960e1f320d5f276
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:47:15 -0500

    unit tests for TrendingTopicClient class

---
+++ clientlibrary/Microsoft.ProjectOxford.Search.Test/TrendingTopicClientTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ProjectOxford.Search.News;
+using System;
+using Xunit;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    /// <summary>
+    /// Unit tests for trending topic client.
+    /// </summary>
+    public class TrendingTopicClientTest
+    {
+        /// <summary>
+        /// Tests the GetTrendingTopicsTest() method.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "TrendingTopics")]
+        public void GetTrendingTopicsTest()
+        {
+            var request = new TrendingTopicRequest();
+
+            var client = new TrendingTopicClient(AppSettings.Instance.SearchApiKey);
+            var response = client.GetTrendingTopics(request);
+
+            Assert.True(response.Results.Count > 0);
+        }
+    }
+}
