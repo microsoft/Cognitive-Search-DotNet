4cac29ee9a2ae8c44105639ac747ba7c94e561b4
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:45:04 -0500

    unit tests for TrendingImageClient class

---
+++ clientlibrary/Microsoft.ProjectOxford.Search.Test/TrendingImageClientTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ProjectOxford.Search.Image;
+using System;
+using Xunit;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    /// <summary>
+    /// Unit tests for the TrendingImageClient class.
+    /// </summary>
+    public class TrendingImageClientTest
+    {
+        /// <summary>
+        /// Tests the GetImages() method.
+        /// </summary>
+        [Fact]
+        [Trait("Category","TrendingImages")]
+        public void GetImagesTest()
+        {
+            var client = new TrendingImageClient(AppSettings.Instance.SearchApiKey);
+
+            var response = client.GetImages();
+
+            Assert.True(response.Categories.Count > 0);
+        }
+    }
+}
