841246205678ae133b76c4eea4177d53659b44ad
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:11:27 -0500

    added AppSettings class

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search.Test/AppSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    public class AppSettings
+    {
+        private static AppSettings _instance;
+
+        public static AppSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new AppSettings();
+
+                return _instance;
+            }
+        }
+
+        public string AutoSuggestApiKey
+        {
+            get
+            {
+                var apiKey = Environment.GetEnvironmentVariable("COG_API_KEY_AUTOSUGGEST");
+
+                if (string.IsNullOrEmpty(apiKey))
+                    throw new Exception("Environment variable COG_API_KEY_AUTOSUGGEST not found.");
+
+                return apiKey;
+            }
+        }
+
+        public string SearchApiKey
+        {
+            get
+            {
+                var apiKey = Environment.GetEnvironmentVariable("COG_API_KEY_SEARCH");
+
+                if (string.IsNullOrEmpty(apiKey))
+                    throw new Exception("Environment variable COG_API_KEY_SEARCH not found.");
+
+                return apiKey;
+            }
+        }
+    }
+}
