1719e2d1b7425546ec7823f854afb1c21dc40ba4
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:18:38 -0500

    unit tests for CategoryNewsClient class

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search.Test/CategoryNewsClientTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ProjectOxford.Search.News;
+using System;
+using Xunit;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    /// <summary>
+    /// Unit tests for category news client.
+    /// </summary>
+    public class CategoryNewsClientTest
+    {
+        /// <summary>
+        /// Tests the GetCategoryNews() method.
+        /// </summary>
+        [Fact]
+        [Trait("Category","CategoryNews")]
+        public void GetCategoryNewsTest()
+        {
+            var request = new CategoryNewsRequest();
+            request.Category = Category.Business;
+
+            var client = new CategoryNewsClient(AppSettings.Instance.SearchApiKey);
+            var response = client.GetCategoryNews(request);
+
+            Assert.True(response.Results.Count > 0);
+        }
+    }
+}
