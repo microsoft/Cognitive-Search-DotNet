ab18ea0a55f2a795bb85b6b48b57646794f0cc95
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:50:32 -0500

    unit tests for TrendingVideoClient class

---
+++ clientlibrary/Microsoft.ProjectOxford.Search.Test/TrendingVideoClientTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ProjectOxford.Search.Video;
+using System;
+using Xunit;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    /// <summary>
+    /// Unit tests for the TrendingVideoClient class.
+    /// </summary>
+    public class TrendingVideoClientTest
+    {
+        /// <summary>
+        /// Tests the GetVideos() method.
+        /// </summary>
+        [Fact]
+        [Trait("Category","TrendingVideos")]
+        public void GetVideosTest()
+        {
+            var client = new TrendingVideoClient(AppSettings.Instance.SearchApiKey);
+
+            var response = client.GetVideos();
+
+            Assert.True(response.BannerTiles.Count > 0);
+            Assert.True(response.Categories.Count > 0);
+        }
+    }
+}
