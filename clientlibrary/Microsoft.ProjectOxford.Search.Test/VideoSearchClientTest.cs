b4e53598e286b58e4eaa259ccfaa27aae70c0aab
Adam Grocholski <adgroc@outlook.com>
2017-08-04 10:56:29 -0500

    unit tests for VideoSearchClient class

---
+++ clientlibrary/Microsoft.ProjectOxford.Search.Test/VideoSearchClientTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.ProjectOxford.Search.Core;
+using Microsoft.ProjectOxford.Search.Video;
+using System;
+using Xunit;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    public class VideoSearchClientTest
+    {
+        /// <summary>
+        /// Tests the Validate() method to ensure a query is specified.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "VideoSearch")]
+        public void ValidateTest_QueryNotSpecified()
+        {
+            var request = new VideoSearchRequest();
+            Assert.Throws<QueryNotSpecifiedException>(() => request.Validate());
+        }
+
+        /// <summary>
+        /// Tests the GetVideos() method.
+        /// </summary>
+        [Fact]
+        [Trait("Category","VideoSearch")]
+        public void GetVideosTest()
+        {
+            var request = new VideoSearchRequest();
+            request.Query = "cats";
+            request.Count = 10;
+            request.Offset = 0;
+            request.Market = "en-US";
+            request.SafeSearch = SafeSearch.Moderate;
+
+            var client = new VideoSearchClient(AppSettings.Instance.SearchApiKey);
+
+            var response = client.GetVideos(request);
+
+            Assert.True(response.Videos.Count > 0);
+        }
+    }
+}
